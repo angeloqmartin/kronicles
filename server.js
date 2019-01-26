0cc077f369f8c13b6f9452be706c20b3d39b04e2
Angelo Martin <angeloqmartin@gmail.com>
2019-01-26 09:04:20 -0800

    created public/app.js file removed mock data handler functions from server.js and added to app.js edited script tag to point to app.js

--- server.js
+++ server.js
@@ -2,75 +2,9 @@
 
 const express = require('express');
 const nodemon = require('nodemon')
-
 const app = express();
+
 app.use(express.static('public'));
-
-
-const mockVentures = {
-    "suggestedVentures": [
-        {
-            "id": "1111111",
-            "locationTitle": "la super park",
-            "address": {
-                "street": "123 nonReal Ln",
-                "city": "Los Angeles",
-                "description": "California",
-                "zipCode": 90001,
-            },
-            "text": "best park in la",
-            "visted": true,
-        },
-        {
-            "id": " ",
-            "locationTitle": "best foods",
-            "address": {
-                "street": "123 nonReal Ln",
-                "city": "Los Angeles",
-                "description": "California",
-                "zipCode": 90001,
-            },
-            "description": "best food in town",
-            "visted": true,
-        },
-        {
-            "id": "3333333",
-            "locationTitle": "la super park",
-            "address": {
-                "street": "123 nonReal Ln",
-                "city": "Los Angeles",
-                "description": "California",
-                "zipCode": 90001,
-            },
-            "text": "best park in la",
-            "visted": true,
-        },
-    ]
-};
-
-function getSuggestedVentures(ventures){
-    setTimeout(() => { 
-        ventures(mockVentures) 
-    });
-};
-
-// function stays same when dealing with real API
-function displaySuggestedVentures(data){
-    for (let index in data.suggestedVentures) {
-        $(body).append(
-            '<p>' + data.suggestedVentures[index].text + '<p>');
-    }
-};
-
-// function stays same when dealing with real API
-function getAndDisplaySuggestedVentures() {
-    getSuggestedVentures(displaySuggestedVentures);
-};
-
-// on page load function should run
-$(function() {
-    getAndDisplaySuggestedVentures();
-});
 
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, function () {
