fe95ee7a07baae8ffcebf44f4306f08b3d703d68
Angelo Martin <angeloqmartin@gmail.com>
2019-01-28 16:47:29 -0800

    refactor mock data

--- public/app.js
+++ public/app.js
@@ -3,38 +3,38 @@ const mockVentures = {
         {
             "id": "1111111",
             "locationTitle": "la super park",
+            "text": "best park in la",
+            "visted": true,
             "address": {
                 "street": "123 nonReal Ln",
                 "city": "Los Angeles",
                 "description": "California",
                 "zipCode": 90001,
             },
-            "text": "best park in la",
-            "visted": true,
         },
         {
             "id": " ",
             "locationTitle": "best foods",
+            "text": "best park in la",
+            "visted": true,
             "address": {
                 "street": "123 nonReal Ln",
                 "city": "Los Angeles",
                 "description": "California",
                 "zipCode": 90001,
             },
-            "description": "best food in town",
-            "visted": true,
         },
         {
             "id": "3333333",
             "locationTitle": "la super park",
+            "text": "best park in la",
+            "visted": true,
             "address": {
                 "street": "123 nonReal Ln",
                 "city": "Los Angeles",
                 "description": "California",
                 "zipCode": 90001,
             },
-            "text": "best park in la",
-            "visted": true,
         },
     ]
 };
