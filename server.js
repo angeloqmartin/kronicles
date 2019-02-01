fe228292148797213d617e99884c18afe1f8889d
Angelo Martin <angeloqmartin@gmail.com>
2019-02-01 12:58:14 -0800

    setup mongoose environment

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 "use strict";
 
+const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost/trips') // this ref the mongoDB installed on localhost
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, function () {
