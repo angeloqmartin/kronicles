b1d1a8981a5c2ebff955e5e496d48056b942ffdd
Angelo Martin <angeloqmartin@gmail.com>
2019-02-01 13:58:38 -0800

    compile tripSchema to modal

--- server.js
+++ server.js
@@ -12,19 +12,25 @@ mongoose.connect('mongodb://localhost/trips')
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const userTripReportSchema = new mongoose.Schema({
-    // objectID: ? do I need this
     locationName: { type: String, required: true }, // 'DISNEYWORLD'
-    originalNameTyped: { type: String, required: true } ,// '  dis ney worl   d'
+    // originalNameTyped: { type: String, required: true } ,// '  dis ney worl   d'
     locationCategory: { type: String, require: true},
-    locationPostalCode: { type: String, require: true},
+    postalCode: { type: String, require: true},
     content: { type: String, required: true },
     isPublished: Boolean
     // rating: { type: Number }, // 1-5 // add rating feature
 })
 
+const Trips = mongoose.model('Trips', userTripReportSchema);
+const trips = new Trips({
+    locationName: "DISNEYWORLD",
+    locationCategory: "outdoors",
+    postalCode: "32830",
+    content:"The Walt Disney World Resort, also called Walt Disney World and Disney World, is an entertainment complex in Bay Lake and Lake Buena Vista, Florida, in the United States, near the cities Orlando and Kissimmee.",
+    isPublished:true
+})
 
 // run test to prevent dup tripReports => // '  dis ney worl   d' // 'DISNEYWORLD'
-
 
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, function () {
