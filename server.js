4ffa9e130dc50f908b8cf1b02ba13e4adc250741
Angelo Martin <angeloqmartin@gmail.com>
2019-02-01 13:31:07 -0800

    created userTripReportSchema

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ const app = express();
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/trips') // this ref the mongoDB installed on localhost
+// this ref the mongoDB installed on localhost
+mongoose.connect('mongodb://localhost/trips')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const userTripReportSchema = new mongoose.Schema({
+    // objectID: ? do I need this
+    locationName: { type: String, required: true }, // 'DISNEYWORLD'
+    originalNameTyped: { type: String, required: true } ,// '  dis ney worl   d'
+    locationCategory: { type: String, require: true},
+    locationPostalCode: { type: String, require: true},
+    content: { type: String, required: true },
+    isPublished: Boolean
+    // rating: { type: Number }, // 1-5 // add rating feature
+})
+
+
+// run test to prevent dup tripReports => // '  dis ney worl   d' // 'DISNEYWORLD'
+
 
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, function () {
