4325dc15e39f521c1c193ea99c7012a9b8965e85
Angelo Martin <angeloqmartin@gmail.com>
2019-02-03 13:19:30 -0800

    compile tripSchema to modal

--- server.js
+++ server.js
@@ -7,50 +7,16 @@ const app = express();
 
 app.use(express.static('public'));
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs
-// so declare `server` here
-// and then assign a value to it in run
-let server;
- 
-// this function connects to our database, then starts the server
-function runServer(databaseUrl, port = PORT) {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
-    });
-  });
-}
-// this function closes the server, and returns a promise. we'll
-// use it in our integration tests later.
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  });
-}
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+
+// this ref the mongoDB installed on localhost
+mongoose.connect('mongodb://localhost/trips')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
 if (require.main === module) {
-  runServer(DATABASE_URL).catch(err => console.error(err));
-}
+    app.listen(process.env.PORT || 8080, function () {
+    });
+};
+
 
 module.exports = app;
-module.exports = { runServer, app, closeServer };
