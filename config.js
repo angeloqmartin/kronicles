1b83feb986b13dd9e879c309c4f460fe94d46e8a
Angelo Martin <angeloqmartin@gmail.com>
2019-02-05 09:06:44 -0800

    CI test failed error app.address is not a function

--- config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/trips';
-exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const {DATABASE_URL, PORT} = require('./config');
 const { TripReport } = require('./model');
 
 const app = express();
@@ -11,49 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, 
-// `server` is declare here and then assign a value to it in run
-let server;
+// this ref the mongoDB installed on localhost
+mongoose.connect('mongodb://localhost/trips')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// connects to our database, then starts server
-function runServer(databaseUrl = DATABASE_URL, port = PORT) {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
-    });
-  });
-}
-
-// closes server, and returns a promise 
-// to use in integration tests
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  });
-}
-
-// if server.js is called directly this block runs
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-}
-
-module.exports = { runServer, app, closeServer };
+  app.listen(process.env.PORT || 8080, function () {
+    console.log(`listening on ${this.address().port}...`)
+  });
+};
+    
+module.exports = app;
