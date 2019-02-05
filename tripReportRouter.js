0da654f512c14091348eae7395c739ad01f22737
Angelo Martin <angeloqmartin@gmail.com>
2019-02-05 12:13:10 -0800

    added router file for tripReports

---
+++ tripReportRouter.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const { TripReport } = require('./model');
+
+async function createCourse() {
+    const trip = new TripReport({
+        locationName: "Six Flags Magic Mountain",
+        postalCode: "91355",
+        content:"It is the largest amusement park company in the world, based on the number of properties owned, and is ranked seventh in terms of attendance.",
+        isPublished:true
+    })
+    const result = await trip.save();
+    console.log(result);
+}
+
+createCourse();
+
+router.get("/", (req, res) => {
+    res.json(TripReport).get();
+})
+
+module.exports = router;
