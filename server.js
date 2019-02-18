3c14ac93934d0ddf7ed08c4533d368a772f71683
Angelo Martin <angeloqmartin@gmail.com>
2019-02-18 09:28:38 -0800

    install npm morgan, passort

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
 mongoose.Promise = global.Promise;
 
-const tripReportRouter = require("./tripReportRouter")
 const app = express();
+app.use(morgan('common'));
 
+const tripReportRouter = require("./tripReportRouter")
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -15,13 +19,13 @@ app.use("/trip-report", tripReportRouter);
 
 // this ref the mongoDB installed on localhost
 mongoose.connect('mongodb://localhost/trips')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function () {
     console.log(`listening on ${this.address().port}...`)
   });
 };
-    
+
 module.exports = app;
